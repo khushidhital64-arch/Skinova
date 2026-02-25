3b3626b93b9ec804f7e828aa7f3784d80b95bbb4
khushidhital64-arch <khushidhital64@gmail.com>
2026-02-25 22:57:32 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,11 @@ import BlogList from "./Pages/BlogPage";
 import BlogDetails from "./Pages/BlogDetails";
 import UserDashboard from "./Pages/UserDashboard";
 import Recommended from "./Pages/Recommended";
+import Userpost from "./Pages/Userpost";
+import PaymentSuccess from "./Payment/PaymentSuccess";
+import Favorite from "./Pages/Favorite";
+import MakeupStore from "./Pages/Test";
+import Chat from "./Pages/Chat";
 
 const App = () => {
   return (
@@ -28,19 +33,21 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/userpost" element={<Userpost />} />
 
+        <Route path="/test" element={<MakeupStore />} />
+
+        <Route path="/paymentsuccess" element={<PaymentSuccess />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<CartPage />} />
-               <Route path="/userdashboard" element={<UserDashboard />} />
-                 <Route path="/recommend" element={<Recommended />} />
+        <Route path="/userdashboard" element={<UserDashboard />} />
+        <Route path="/recommend" element={<Recommended />} />
+         <Route path="/chat" element={<Chat />} />
+
+        <Route path="/Favorite" element={<Favorite />} />
 
         <Route path="/blogs" element={<BlogList />} />
         <Route path="/blogs/:id" element={<BlogDetails />} />
-
-
-
-
-
 
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/analytics" element={<Analytics />} />
